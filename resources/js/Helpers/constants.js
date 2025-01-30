5c08245f2223b12f2ed9377f0d8edcaa2b84c0f6
Allan Kong <allankong176@gmail.com>
2025-01-29 20:45:37 -0700

    Refactored form into a mandatory form component

---
+++ resources/js/Helpers/constants.js
@@ -0,0 +1,29 @@
+export const resourceTypes = [
+    { label: "Website", value: "website" },
+    { label: "Book", value: "book" },
+    { label: "Blog", value: "blog" },
+    { label: "Course", value: "course" },
+    { label: "Bootcamp", value: "bootcamp" },
+    { label: "Youtube Channel", value: "youtube" },
+    { label: "Newsletter", value: "newsletter" },
+    { label: "Podcast", value: "podcast" },
+    { label: "Forum", value: "forum" },
+    { label: "Workshop", value: "workshop" },
+    { label: "Mobile app", value: "mobile_app" },
+    { label: "Desktop app", value: "desktop_app" },
+    { label: "E-zine", value: "e_zine" }
+];
+
+export const pricingOptions = [
+    { label: "Free", value: "free" },
+    { label: "Paid", value: "paid" },
+    { label: "Freemium", value: "freemium" }
+];
+
+export const difficultyLevels = [
+    { label: "Beginners - Non-technical", value: "beginners" },
+    { label: "Industry Simple - Jr Engineer", value: "industry_simple" },
+    { label: "Industry Standard - Engineer", value: "industry_standard" },
+    { label: "Industry Professional - Senior Engineer", value: "industry_professional" },
+    { label: "Academic - Research", value: "academic" }
+];
