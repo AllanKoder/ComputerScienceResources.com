81af657deae2cae5522b35223542409a9d87cbae
AllanKoder <74692833+AllanKoder@users.noreply.github.com>
2025-08-01 03:02:43 +0000

    Apply automatic changes

--- app/Models/NewsPost.php
+++ app/Models/NewsPost.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
 
 class NewsPost extends Model
 {
     use Sluggable;
 
     protected $fillable = ['title', 'cover_image_path', 'excerpt', 'content'];
+
     /**
      * Return the sluggable configuration array for this model.
      */
