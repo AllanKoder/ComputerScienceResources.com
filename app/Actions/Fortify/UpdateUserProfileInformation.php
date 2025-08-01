81b6e7c57c3f3843f65930cac323eab590199274
AllanKoder <74692833+AllanKoder@users.noreply.github.com>
2025-08-01 01:35:03 +0000

    Apply automatic changes

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -3,9 +3,7 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
 
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
