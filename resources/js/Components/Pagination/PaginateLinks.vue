977e62e48be09468dfb9203afd3951b94e33bd59
Allan Kong <allankong176@gmail.com>
2025-08-05 16:59:07 -0700

    default: 0

--- resources/js/Components/Pagination/PaginateLinks.vue
+++ resources/js/Components/Pagination/PaginateLinks.vue
@@ -10,11 +10,11 @@ const props = defineProps({
     },
     from: {
         type: Number,
-        required: true,
+        default: 0,
     },
     to: {
         type: Number,
-        required: true,
+        default: 0,
     },
     total: {
         type: Number,
@@ -33,7 +33,7 @@ const nextLink = props.links[props.links.length - 1]?.url;
 
 <template>
     <nav class="flex flex-col items-center justify-center space-y-2">
-        <span class="text-sm text-gray-700 dark:text-gray-400">
+        <span class="text-sm text-gray-700 mt-4 dark:text-gray-400">
             <template v-if="total === 0">
                 No results found for {{ modelName }}.
             </template>
@@ -41,7 +41,7 @@ const nextLink = props.links[props.links.length - 1]?.url;
                 Showing <span class="font-semibold">{{ from ?? 0 }}</span> to <span class="font-semibold">{{ to ?? 0 }}</span> of <span class="font-semibold">{{ total ?? 0 }}</span> {{ modelName }}
             </template>
         </span>
-        <div v-if="total > 0" class="flex items-center space-x-1.5">
+        <div class="flex items-center space-x-1.5">
             <!-- Left Button -->
             <PaginateLink
                 v-if="previousLink"
