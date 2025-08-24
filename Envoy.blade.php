1de78f6bfb293d8517cf278de8449c7d781c9c62
Allan Kong <allankong176@gmail.com>
2025-08-23 18:13:16 -0700

    Favicon

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,8 +1,6 @@
-@servers(['local' => ['127.0.0.1'], 'server' => [getenv('DEPLOY_USER') . '@' . getenv('DEPLOY_HOST')]])
+@servers(['local' => ['127.0.0.1'], 'server' => ['root@143.198.129.111']])
 
-{{-- Add Caching, for production app --}}
 @story('deploy', ['skipBackup' => false])
-    prepare-frontend-locally
     @if(!$skipBackup)
         backup-database
     @endif
@@ -46,50 +44,16 @@
 
 @task('perform-migration', ['on' => 'server'])
     set -e
-    cd {{ $DEPLOY_PATH }}
-    php artisan down
-@endtask
-
-@task('up', ['on' => 'server'])
-    set -e
-    cd {{ $DEPLOY_PATH }}
-    php artisan up
-@endtask
-
-@task('backup-database', ['on' => 'server'])
-    set -e
-    cd {{ $DEPLOY_PATH }}
-    php artisan backup:run --only-db
-@endtask
-
-@task('update-code', ['on' => 'server'])
-    set -e
-    cd {{ $DEPLOY_PATH }}
-    git pull origin master
-@endtask
-
-@task('install-dependencies', ['on' => 'server'])
-    set -e
-    cd {{ $DEPLOY_PATH }}
-    composer install --no-dev --optimize-autoloader
-@endtask
-
-@task('perform-migration', ['on' => 'server'])
-    set -e
-    cd {{ $DEPLOY_PATH }}
+    cd /var/www/ComputerScienceResources.com
     php artisan migrate --force
-@endtask
-
-@task('prepare-frontend-locally', ['on' => 'local'])
-    set -e
-    echo "Building frontend locally..."
-    npm ci
-    npm run build
-    echo "Frontend build complete. Ready to deploy."
 @endtask
 
 @task('deploy-frontend', ['on' => 'local'])
     set -e
+    echo "Building frontend locally..."
+    npm ci
+    npm run build
+
     echo "Copying compiled assets to server..."
     scp -r {{ __DIR__ }}/public/build root@143.198.129.111:/var/www/ComputerScienceResources.com/public
 @endtask
