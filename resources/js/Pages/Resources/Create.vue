2b54e09d706255d05a2b1a7fe2c2f7823464c7fd
Allan Kong <allankong176@gmail.com>
2025-08-26 10:52:28 -0700

    Guidelines

--- resources/js/Pages/Resources/Create.vue
+++ resources/js/Pages/Resources/Create.vue
@@ -12,6 +12,7 @@ import { useLocalStorageSaver } from "@/Composables/useLocalStorageSaver";
 import ConfirmationModal from "@/Components/ConfirmationModal.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 import DangerButton from "@/Components/DangerButton.vue";
+import SubmissionGuidelines from '../../Components/SubmissionGuidelines.vue'
 
 const formFields = [
     "name",
@@ -107,130 +108,11 @@ const handleFormChange = (newFormData) => {
         <main class="py-12 flex justify-center">
             <div class="w-full flex flex-col md:flex-row gap-10 justify-center items-start">
                 <!-- Instructions Sidebar (Rules) -->
-                <div
-                    class="bg-gray-50 border border-gray-200 rounded-lg p-6 w-full md:w-[28vw] min-w-[20rem] h-fit sticky md:top-8 mt-0 md:mb-0"
-                >
-                    <h3 class="text-lg font-semibold mb-4 text-gray-800">
-                        Submission Guidelines
-                    </h3>
-                    <div class="space-y-4 text-sm text-gray-700">
-                        <div class="space-y-4 text-sm text-gray-700">
-                            <div>
-                                <h4 class="font-bold text-gray-800 mb-2">
-                                    Resource Scope
-                                </h4>
-                                <p class="mb-2">
-                                    This site features comprehensive learning
-                                    resources rather than isolated materials.
-                                    Resources should provide ongoing value or
-                                    structured learning experiences rather than
-                                    a single-use reference.
-                                </p>
-                                <p>
-                                    Our goal is to provide ways for developers
-                                    to hone their skills. This industry is
-                                    filled with passion, so we should make it
-                                    easier to find more ways to learn.
-                                </p>
-                            </div>
-
-                            <div>
-                                <h4 class="font-bold text-gray-800 mb-2">
-                                    Types of Content Accepted
-                                </h4>
-                                <ul
-                                    class="list-disc list-inside space-y-1 text-xs"
-                                >
-                                    <li>
-                                        Platforms, websites, and tools that
-                                        offer interactive learning
-                                    </li>
-                                    <li>
-                                        Collections of educational content, such
-                                        as YouTube channels or book series
-                                    </li>
-                                    <li>
-                                        Guides or repositories that serve as
-                                        long-term learning hubs
-                                    </li>
-                                    <li>
-                                        Newsletters that consistently release
-                                        content to date
-                                    </li>
-                                    <li>
-                                        Organizations that can provide software
-                                        career advising
-                                    </li>
-                                </ul>
-                                <p class="mt-2 text-xs">
-                                    Anything to help people learn more about
-                                    software: from hardware, system design, to
-                                    project management. The more specialized the
-                                    resources are, the better.
-                                </p>
-                            </div>
-
-                            <div>
-                                <h4 class="font-bold text-gray-800 mb-2">
-                                    Exceptions
-                                </h4>
-                                <ul
-                                    class="list-disc list-inside space-y-1 text-xs"
-                                >
-                                    <li>
-                                        An individual book may be included if it
-                                        is exceptionally well-regarded and
-                                        widely recommended as a foundational
-                                        resource
-                                    </li>
-                                    <li>
-                                        Entertainment streamers and YouTubers
-                                        can be included given that they are very
-                                        popular whilst still informative
-                                    </li>
-                                    <li>
-                                        Do not post your paid courses unless
-                                        they are well received - this is not a
-                                        platform to advertise unwanted courses
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div>
-                                <h4 class="font-bold text-gray-800 mb-2">
-                                    What's Not Included
-                                </h4>
-                                <ul
-                                    class="list-disc list-inside space-y-1 text-xs"
-                                >
-                                    <li>
-                                        Standalone videos, single blog posts, or
-                                        one-off articles
-                                    </li>
-                                    <li>
-                                        Resources that are too broad and do not
-                                        contain a singular focus
-                                    </li>
-                                    <li>
-                                        Things not related to learning about
-                                        computer science or software engineering
-                                    </li>
-                                    <li>
-                                        Lifestyle or personal finance content
-                                        (beyond reasonable project management)
-                                    </li>
-                                </ul>
-                                <p class="mt-2 text-xs italic">
-                                    In the end, we trust you to be reasonable.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <SubmissionGuidelines />
                 <!-- Main Form Section -->
                 <div
                     ref="formRef"
-                    class="bg-white h-min shadow-lg rounded-lg p-6 flex-1 max-w-full md:max-w-3xl min-w-0 md:min-w-[28rem] relative"
+                    class="bg-white h-min shadow-lg rounded-lg p-5 relative overflow-auto w-full md:w-[46vw] max-w-4xl min-w-[28rem]"
                     id="create-resource-form"
                 >
                     <!-- Move Reset button to top right -->
